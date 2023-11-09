4b2c14c6adedd9792714665fe07a1af5076ea14c
Angel RN <angelrodean@gmail.com>
2023-11-09 15:00:04 -0600

    Fix movil search

--- auto-search.py
+++ auto-search.py
@@ -7,31 +7,34 @@ import pyautogui
 import pathlib
 import time
 
-WORDLIST_PATH = pathlib.Path(__file__).parent / 'wordlist.txt'
-WORDLIST = WORDLIST_PATH.read_text().splitlines()
+def dictionary():
+    worlist_path = pathlib.Path(__file__).parent / 'wordlist.txt'
+    wordlist = worlist_path.read_text().splitlines()
+    return wordlist
 
-def search(driver):
-    for i, word in enumerate(WORDLIST):
-        try:
-            elem = driver.find_element(By.NAME, "q")
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "q")))
-            elem.clear()
-            elem.send_keys(word)
-            elem.send_keys(Keys.RETURN)
-            if "No results found." in driver.page_source:
-                print(f"No se encontraron resultados para '{word}'.")
-            i+=1
-        except Exception as e:
-            print(f"Error al buscar '{word}': {e}")
-    return i
+def search(driver, word):
+    try:
+        elem = driver.find_element(By.NAME, "q")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "q")))
+        elem.clear()
+        elem.send_keys(word)
+        elem.send_keys(Keys.RETURN)
+        if "No results found." in driver.page_source:
+            print(f"No se encontraron resultados para '{word}'.")
+    except Exception as e:
+        print(f"Error al buscar '{word}': {e}")
 
 def search_desktop(driver):
     driver.get("https://www.bing.com/")
     assert "Bing" in driver.title
     time.sleep(5)
-    num_search = search(driver)
+    
+    wordlist = dictionary()
+    for i, word in enumerate(wordlist):
+        search(driver,word)
+        i+=1
 
-    return print(f"Busquedas Desktop: {num_search}")
+    return print(f"Busquedas Desktop: {i}")
 
 
 def search_movil(driver):
@@ -43,12 +46,16 @@ def search_movil(driver):
     time.sleep(3)
     pyautogui.hotkey('ctrl', 'shift', 'm')
 
-    num_search = search(driver)
+    wordlist = dictionary()
+    for i, word in enumerate(wordlist):
+        search(driver,word)
+        i+=1
+        if i == 23:
+            break
 
-    return print(f"Busquedas Movil: {num_search}")
+    return print(f"Busquedas Movil: {i}")
 
-
-def main():
+if __name__ == '__main__':
     OPTIONS = webdriver.EdgeOptions()
     OPTIONS.add_experimental_option("excludeSwitches", ["enable-logging"])
 
@@ -56,6 +63,3 @@ def main():
         search_desktop(driver)
         search_movil(driver)
     driver.quit()
-
-if __name__ == '__main__':
-    main()
